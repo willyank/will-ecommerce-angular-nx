75e8749b74e2a7f93e7d2aeb9e73095bd9766aaa
willyan klumb <wklumb@grupogbi.com>
2021-10-05 08:45:58 -0300

    teste import styl e button GOL

---
+++ libs/ui/src/lib/gol-button/gol-button.component.ts
@@ -0,0 +1,54 @@
+﻿import { Component, ElementRef, Input, ViewChild } from '@angular/core';
+
+@Component({
+  selector: 'gol-button',
+  templateUrl: './gol-button.component.html',
+  styleUrls: ['./gol-button.component.styl'],
+})
+export class GolButtonComponent {
+  @ViewChild('button') button: ElementRef;
+
+  @Input() onClick: Function;
+
+  @Input() customClass = '';
+
+  @Input() id = '';
+
+  @Input() isLower = false;
+
+  @Input() loading = false;
+
+  @Input() isBlock = false;
+
+  @Input() isJust = false;
+
+  @Input() isDisabled = false;
+
+  @Input() isDisabledOpacity = false;
+
+  @Input() isButtonOrange = false;
+
+  @Input() isBright = false;
+
+  @Input() mobileFull = false;
+
+  @Input() isTransparent = false;
+
+  @Input() isBgTransparent = false;
+
+  @Input() type = 'button';
+
+  classLoading = false;
+
+  clickLoad(): void {
+    this.classLoading = true;
+    setTimeout(() => {
+      this.successLoad();
+    }, 3000);
+  }
+
+  successLoad(): void {
+    this.classLoading = false;
+    this.button.nativeElement.blur();
+  }
+}
--- libs/ui/src/lib/ui.module.ts
+++ libs/ui/src/lib/ui.module.ts
@@ -1,11 +1,12 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { BannerComponent } from './banner/banner.component';
+import { GolButtonComponent } from './gol-button/gol-button.component';
 import { SliderComponent } from './slider/slider.component';
 
 @NgModule({
   imports: [CommonModule],
-  declarations: [BannerComponent, SliderComponent],
-  exports: [BannerComponent, SliderComponent],
+  declarations: [BannerComponent, SliderComponent, GolButtonComponent],
+  exports: [BannerComponent, SliderComponent, GolButtonComponent],
 })
 export class UiModule {}
