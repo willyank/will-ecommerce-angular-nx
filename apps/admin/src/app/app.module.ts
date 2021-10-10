d1468abed09f4d39379277e731b209443fdcf6a5
willyan klumb <wklumb@grupogbi.com>
2021-10-10 15:06:13 -0300

    base organizada

--- apps/admin/src/app/app.module.ts
+++ apps/admin/src/app/app.module.ts
@@ -2,15 +2,37 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { RouterModule } from '@angular/router';
+import { RouterModule, Routes } from '@angular/router';
+import { ShellComponent } from './shared/shell/shell.component';
+import { SidebarComponent } from './shared/sidebar/sidebar.component';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
+
+const rountes: Routes = [
+  {
+    path: '',
+    component: ShellComponent,
+    children: [
+      {
+        path: 'dashboard',
+        component: DashboardComponent,
+      },
+    ],
+  },
+];
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [
+    AppComponent,
+    DashboardComponent,
+    ShellComponent,
+    SidebarComponent,
+  ],
   imports: [
     BrowserModule,
-    RouterModule.forRoot([], { initialNavigation: 'enabledBlocking' }),
+    RouterModule.forRoot(rountes, { initialNavigation: 'enabledBlocking' }),
   ],
   providers: [],
   bootstrap: [AppComponent],
+  exports: [],
 })
 export class AppModule {}
--- libs/ui/src/lib/ui.module.ts
+++ libs/ui/src/lib/ui.module.ts
@@ -1,12 +1,11 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { BannerComponent } from './banner/banner.component';
-import { GolButtonComponent } from './gol-button/gol-button.component';
 import { SliderComponent } from './slider/slider.component';
 
 @NgModule({
   imports: [CommonModule],
-  declarations: [BannerComponent, SliderComponent, GolButtonComponent],
-  exports: [BannerComponent, SliderComponent, GolButtonComponent],
+  declarations: [BannerComponent, SliderComponent],
+  exports: [BannerComponent, SliderComponent],
 })
 export class UiModule {}
