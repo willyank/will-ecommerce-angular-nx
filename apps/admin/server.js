82053dd5ff03451b985c660ac0efbf1ea77eba54
willyan klumb <wklumb@grupogbi.com>
2021-10-16 15:07:10 -0300

    process.env.APP_NAME

--- apps/admin/server.js
+++ apps/admin/server.js
@@ -5,10 +5,10 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static('./dist/apps/admin'));
+app.use(express.static(`./dist/apps/${process.env.APP_NAME}`));
 
 app.get('/*', (req, res) =>
-  res.sendFile('index.html', { root: 'dist/apps/admin/' })
+  res.sendFile('index.html', { root: `dist/apps/${process.env.APP_NAME}/` })
 );
 
 // Start the app by listening on the default Heroku port
