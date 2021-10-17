144ded0fde3f8d46dfc0b0eada4399c30b9cb8f1
willyan klumb <wklumb@grupogbi.com>
2021-10-17 08:09:25 -0300

    versao no ngstore

--- apps/ngstore/src/app/shared/footer/footer.component.ts
+++ apps/ngstore/src/app/shared/footer/footer.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { environment } from '@env/environment';
 
 @Component({
   selector: 'willyan-company-footer',
@@ -6,7 +7,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./footer.component.scss'],
 })
 export class FooterComponent implements OnInit {
-  constructor() {}
+  version: string;
+  constructor() {
+    this.version =
+      'Versão: ' + environment.version + ' <br> ' + environment.versionDate;
+  }
 
   ngOnInit(): void {}
 }
