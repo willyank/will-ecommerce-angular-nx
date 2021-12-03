10b61bf577d61aca422f9694e588df017c66cdd1
willyan klumb <wklumb@grupogbi.com>
2021-12-03 09:13:18 -0300

    corrigindo url de prod

--- environments/environment.prod.ts
+++ environments/environment.prod.ts
@@ -2,5 +2,5 @@ export const environment = {
   version: require('../package.json').version,
   versionDate: require('../package.json').versionDate,
   production: true,
-  adminApiUrl: 'https://will-ecommerce-webapiadmin.azurewebsites.net/api/v1/',
+  adminApiUrl: 'https://will-ecommerce-webapiadmin.azurewebsites.net/api/',
 };
